ad8f4bb83c9d2ad9b6937f648f6673e205da77eb
bmilojkovic <bandza88@gmail.com>
2025-09-17 12:08:34 +0200

    Added support for weapons, pets and elements. There is a dummy hover panel showing details about boons and stuff as well.

--- src/App.js
+++ src/App.js
@@ -13,17 +13,63 @@ function SmartImage({ src, fallback, style }) {
     ref.current.src = fallback;
   }
 
-  return <img ref={ref} src={src} onError={handleFallback} class="BoonImage"/>;
+  return <img ref={ref} src={src} onError={handleFallback} className="BoonImage"/>;
 }
 
-function BoonPanel({boonName, boonRarity="Common"}) {
-  
+function ElementPanel({elementalData}) {
+  const emptyElementalData = [
+    {name: "Earth", value: 0},
+    {name: "Water", value: 0},
+    {name: "Air", value: 0},
+    {name: "Fire", value: 0},
+    {name: "Aether", value: 0}
+  ];
+
+  return (
+    <div className="ElementPanel">
+      {elementalData != null ? elementalData.map((element) =>
+        <span><span>{element.value}</span><img className="ElementImage" src={`img/Element_${element.name}.png`}/>&nbsp;</span>
+      )
+      :
+      emptyElementalData.map((element) =>
+        <span><span>{element.value}</span><img className="ElementImage" src={`img/Element_${element.name}.png`}/>&nbsp;</span>
+      )}
+    </div>
+  )
+}
+
+function BoonDetail({boonName, boonDescription, boonEffects}) {
+  return (
+    <div className='BoonDetail'>
+      <h1>{boonName}</h1>
+      <p>{boonDescription}</p>
+      <ul>
+        {boonEffects.map((boonEffect, i) =>
+          <li key={`${boonName}Effect${i}`}>{boonEffect}</li>
+        )}
+      </ul>
+    </div>
+  )
+}
+
+function BoonPanel({boonName, boonRarity="Common", boonDescription}) {
+  const [isHovering, setIsHovering] = useState(false);
+  const handleMouseOver = () => {
+    setIsHovering(true);
+  };
+
+  const handleMouseOut = () => {
+    setIsHovering(false);
+  };
+
   return (
     <div
+      onMouseOver={handleMouseOver}
+      onMouseOut={handleMouseOut}
       className="BoonPanel"
       style={!boonName.startsWith("Empty") ? {
         border: "10px solid transparent",
-        borderImage: `url(\"img/Frame${boonRarity}.png\")`,
+        borderImageSource: `url("img/Frame${boonRarity}.png")`,
         borderImageSlice: "25",
         borderImageWidth: "25px",
         borderImageRepeat: "fill",
@@ -41,61 +87,75 @@ function BoonPanel({boonName, boonRarity="Common"}) {
           src={`img/${boonName}.png`}
           fallback="img/DefaultBoon.png"
         />
-      </div>   
+        {isHovering && !boonName.startsWith("Empty") && <BoonDetail boonName={boonName} boonDescription={boonDescription} boonEffects={["Effect1", "Effect2"]}/>}
+      </div>
   );
 }
 
 function App() {
-  const [isSubscribed, setSubscribed] = useState(false)
-
+  const [weaponData, setWeaponData] = useState({})
+  const [familiarData, setFamiliarData] = useState({})
   const [allBoons, setAllBoons] = useState({})
+  const [elementalData, setElementalData] = useState(null)
 
   useEffect(() => {
-    if (!isSubscribed) {
-      setSubscribed(true);
-      var twitch = window.Twitch.ext;
-      twitch.listen("broadcast", function(target, contentType, message) {
-        setAllBoons(JSON.parse(message));
-        console.log(message);
-      });
-    }
-  });
+    var twitch = window.Twitch.ext;
+    twitch.listen("broadcast", function(target, contentType, message) {
+      var runData = JSON.parse(message);
+      setAllBoons(runData.boonData);
+      setWeaponData(runData.weaponData);
+      setFamiliarData(runData.familiarData);
+      setElementalData(runData.elementalData);
+      console.log(message);
+    });
+  }, []);
   
   return (
     <div className="App">
+      <ElementPanel elementalData={elementalData} />
       <ul>
-        <li>
+        {weaponData != null && weaponData.name != null ?
+          <li key="WeaponKey">
+            <BoonPanel boonName={weaponData.name} boonRarity={weaponData.rarity} boonDescription={weaponData.description}/>
+          </li>
+        : ""}
+        {familiarData != null && familiarData.name != null ?
+          <li key="FamiliarKey">
+            <BoonPanel boonName={familiarData.name}/>
+          </li>
+        : ""}
+        <li key="WeaponBoonKey">
           <BoonPanel
             boonName={"weaponBoon" in allBoons ? allBoons.weaponBoon.name : "EmptyWeaponBoon"}
             boonRarity={"weaponBoon" in allBoons ? allBoons.weaponBoon.rarity : ""}
           />
         </li>
-        <li>
+        <li key="SpecialBoonKey">
           <BoonPanel
             boonName={"specialBoon" in allBoons ? allBoons.specialBoon.name : "EmptySpecialBoon"}
             boonRarity={"specialBoon" in allBoons ? allBoons.specialBoon.rarity : ""}
           />
         </li>
-        <li>
+        <li key="CastBoonKey">
           <BoonPanel
             boonName={"castBoon" in allBoons ? allBoons.castBoon.name : "EmptyCastBoon"}
             boonRarity={"castBoon" in allBoons ? allBoons.castBoon.rarity : ""}
           />
         </li>
-        <li>
+        <li key="SprintBoonKey">
           <BoonPanel
             boonName={"sprintBoon" in allBoons ? allBoons.sprintBoon.name : "EmptySprintBoon"}
             boonRarity={"sprintBoon" in allBoons ? allBoons.sprintBoon.rarity : ""}
           />
         </li>
-        <li>
+        <li key="ManaBoonKey">
           <BoonPanel
             boonName={"manaBoon" in allBoons ? allBoons.manaBoon.name : "EmptyManaBoon"}
             boonRarity={"manaBoon" in allBoons ? allBoons.manaBoon.rarity : ""}
           />
         </li>
         {allBoons.otherBoons != null ? allBoons.otherBoons.map((boon) => 
-          <li>
+          <li key={`${boon.name}Key`}>
             <BoonPanel boonName={boon.name} boonRarity={boon.rarity} />      
           </li>
         ) : ""}
