97cbd96b27d17fefdf977cc9ea9d7210d7342cf5
bmilojkovic <bandza88@gmail.com>
2025-09-29 12:50:54 +0200

    Added support for receiving partial data from backend.

--- src/App.js
+++ src/App.js
@@ -16,35 +16,129 @@ function App() {
     allBoons: {},
     weaponData: null,
     familiarData: null,
-    elementalData: null,
+    elementalData: [],
     extraData: null,
     pinBoons: null
   });
 
-  useEffect(() => {
-    var twitch = window.Twitch.ext;
-    twitch.listen("broadcast", function(target, contentType, message) {
-      var runData = JSON.parse(message);
-      
-      if (runData.boonData != null) {
-        dispatch({type: "allBoons", data: runData.boonData});
-      }
-      if (runData.weaponData != null) {
-        dispatch({type: "weapon", data: runData.weaponData});
-      }
-      if (runData.familiarData != null) {
-        dispatch({type: "familiar", data: runData.familiarData});
-      }
-      if (runData.extraData != null) {
-        dispatch({type: "extra", data: runData.extraData});
-      }
-      if (runData.elementalData != null) {
-        dispatch({type: "elemental", data: runData.elementalData});
-      }
-      if (runData.pinData != null) {
-        dispatch({type: "pin", data: runData.pinData});
+  var messageHistory = [];
+
+  function updateState(fullMessage) {
+    var runData = JSON.parse(fullMessage);
+    
+    if (runData.boonData != null) {
+      dispatch({type: "allBoons", data: runData.boonData});
+    }
+    if (runData.weaponData != null) {
+      dispatch({type: "weapon", data: runData.weaponData});
+    }
+    if (runData.familiarData != null) {
+      dispatch({type: "familiar", data: runData.familiarData});
+    }
+    if (runData.extraData != null) {
+      dispatch({type: "extra", data: runData.extraData});
+    }
+    if (runData.elementalData != null) {
+      dispatch({type: "elemental", data: runData.elementalData});
+    }
+    if (runData.pinData != null) {
+      dispatch({type: "pin", data: runData.pinData});
+    }
+  }
+
+  function addMessagePart(nonce, partInd, totalParts, partData) {
+    var foundNonce = false;
+
+    messageHistory.forEach(oldMessage => {
+      if (oldMessage.nonce === nonce) {
+        foundNonce = true;
+        if (oldMessage.archived) {
+          return;
+        }
+
+        if (!(partInd in oldMessage.parts)) {
+          //add part
+          oldMessage.parts[partInd] = partData;
+
+          if (Object.keys(oldMessage.parts).length === oldMessage.totalParts) {
+            //all parts of a non-archived message have arrived
+            var fullMessage = "";
+
+            for (var i = 0; i < oldMessage.totalParts; i++) {
+              fullMessage += oldMessage.parts[i];
+            }
+
+            updateState(fullMessage);
+          }
+        }
       }
     });
+
+    if (!foundNonce) {
+      //message is new. archive the rest, and make this the primary one
+      messageHistory.forEach(oldMessage => {
+        oldMessage.archived = true;
+      });
+
+      var newMessage = {
+        nonce: nonce,
+        totalParts: totalParts,
+        parts: {},
+        archived: false
+      }
+      newMessage.parts[partInd] = partData;
+      messageHistory.push(newMessage);
+
+      if (newMessage.totalParts === 1) {
+        updateState(partData);
+      }
+    }
+  }
+
+  function parseArgusMessageParam(message, startInd, partName) {
+    var paramEndPosition = message.indexOf("*", startInd);
+    if (paramEndPosition === -1) {
+      console.log("Got message in bad format. Couldn't find end " + partName + " star.");
+      return [null, null];
+    }
+    var valueToReturn = parseInt(message.substring(startInd, paramEndPosition));
+    if (Number.isNaN(valueToReturn)) {
+      console.log("Got message in bad format. Couldn't parse " + partName);
+      return [null, null];
+    }
+
+    return [valueToReturn,paramEndPosition];
+  }
+
+  function twitchListen(target, contentType, message) {
+    if (!message.startsWith("*")) {
+      console.log("Got message in bad format. Missing initial star.");
+      return;
+    }
+    var [nonce, nonceEnd] = parseArgusMessageParam(message, 1, "nonce");
+    if (nonce == null) {
+      return;
+    }
+    var [partInd, partIndEnd] = parseArgusMessageParam(message, nonceEnd+1, "part index");
+    if (partInd == null) {
+      return;
+    }
+    var [totalParts, totalPartsEnd] = parseArgusMessageParam(message, partIndEnd+1, "total parts");
+    if (totalParts == null) {
+      return;
+    }
+    
+    console.log("nonce: " + nonce + ", part index: " + partInd + ", total parts:" + totalParts);
+
+    var partData = message.substring(totalPartsEnd+1);
+
+    addMessagePart(nonce, partInd, totalParts, partData);
+    
+  }
+
+  useEffect(() => {
+    var twitch = window.Twitch.ext;
+    twitch.listen("broadcast", twitchListen);
   }, []);
 
   const handleTabClick = (tabId) => {
