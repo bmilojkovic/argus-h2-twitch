3ba210b88aba67a77c5454ffedf6d6fcc6bfa581
bmilojkovic <bandza88@gmail.com>
2025-10-07 16:35:56 +0200

    Setting things up for twitch hosting of frontend.

---
+++ src/ConfigPage.js
@@ -0,0 +1,12 @@
+import "./css/ConfigPage.css";
+
+function ConfigPage() {
+  return (
+    <div className="ConfigPage">
+      <h1>You can see the preview of the extension on the live config page.</h1>
+      <img class="ArgusIcon" src="img/argus_icon.png" alt="Argus Icon" />
+    </div>
+  );
+}
+
+export default ConfigPage;
